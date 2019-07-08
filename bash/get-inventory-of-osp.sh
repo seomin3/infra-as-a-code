25fd8d6eb5ff6f77fd7b23c190e71ff11513341f
Jahyeon Koo <kjahyeon@gmail.com>
2019-07-08 11:19:02 +0900

    various scripts

---
+++ bash/get-inventory-of-osp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IFS='
+'
+
+for i in $(nova list); do
+name=$(echo $i| awk '{ print $4 }')
+ipaddr=$(echo $i| egrep '192.168.([0-9.]{1,3}){2}' -o)
+echo "${name} ansible_ssh_host=${ipaddr}"
+done
+
